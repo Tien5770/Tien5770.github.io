74b3d57d3ee09753442dee4374629e49df022ea7
Tien5770 <txvhuiu@163.com>
2023-04-12 16:51:45 +0800

    Site updated: 2023-04-12 16:51:44

---
+++ js/baidu-tongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?fe67470975a39feb91f9d59afeab846c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
